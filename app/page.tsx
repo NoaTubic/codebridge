c298ec2d257e436b200f7f96a6c1fd7ba3808995
Noa Tubic <ntubic@4ofthem.eu>
2024-12-28 09:12:41 +0100

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
-const  Home = ()  =>  (
-<h1 className="text-3xl text-white font-black">Welcome to the world of Next.js</h1>
-  );
+const Home = () => (
+    <h1 className="text-3xl text-white font-black">Welcome to the world of Next.js</h1>
+);
 
 
 export default Home;
