55edfb130cf44217211dd840abba55676ddcb10a
Noa Tubic <ntubic@4ofthem.eu>
2024-12-30 18:11:31 +0100

    Assets and Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,11 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "CodeBridge",
-  description: "Better version of StackOverflow :D",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
